76c4005481e305c23260aad15ecdd07a4bbdbc6b
kwerulik <kwerulik@gmail.com>
2026-03-13 15:02:18 +0100

    Working backend

---
+++ backend/main.py
@@ -0,0 +1,89 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+
+app = FastAPI(title="Inbank Decision Engine API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MIN_AMOUNT = 2000
+MAX_AMOUNT = 10000
+MIN_PERIOD = 12
+MAX_PERIOD = 60
+
+# Hard coded results
+MOCK_REGISTRY = {
+    "49002010965": {"debt": True, "modifier": 0},
+    "49002010976": {"debt": False, "modifier": 100},
+    "49002010987": {"debt": False, "modifier": 300},
+    "49002010998": {"debt": False, "modifier": 1000},
+}
+
+
+# Pydantic models for request and response
+class LoanApplication(BaseModel):
+    loan_amount: int = Field(ge=MIN_AMOUNT, le=MAX_AMOUNT)
+    loan_period: int = Field(ge=MIN_PERIOD, le=MAX_PERIOD)
+    personal_code: str
+
+class LoanResponse(BaseModel):
+    approved: bool
+    amount: int | None = None
+    period: int | None = None
+    message: str
+
+@app.post("/api/decision", response_model=LoanResponse)
+def make_decision(application: LoanApplication):
+    '''
+        cred_score < 1 ==> not approved
+        cred_score >= 1
+
+    '''
+    user_data = MOCK_REGISTRY.get(application.personal_code)
+
+    if not user_data:
+        raise HTTPException(status_code=404, detail="Personal code not found")
+    
+    if user_data["debt"]:
+        return LoanResponse(
+            approved=False,
+            message="Loan denied due to existing debt."
+        )
+    
+    modifier = user_data["modifier"]
+
+    def calc_max_amount(mod: int, period: int) -> int:
+        return mod * period
+    
+    max_possible_amount = calc_max_amount(modifier, application.loan_period)
+
+    approved_amount = min(max_possible_amount, MAX_AMOUNT)
+
+    if approved_amount >= MIN_AMOUNT:
+        
+        return LoanResponse(
+            approved=True,
+            amount=approved_amount,
+            period=application.loan_period,
+            message="Loan approved"
+        )
+    
+    for new_period in range(application.loan_period + 1, MAX_PERIOD + 1):
+        new_max_amount = calc_max_amount(modifier, new_period)
+        if new_max_amount >= MIN_AMOUNT:
+            return LoanResponse(
+                approved=True,
+                amount=min(new_max_amount, MAX_AMOUNT),
+                period=new_period,
+                message="Requested period too short for minimum loan amount. Adjusted to a new suitable period"
+            )
+    return LoanResponse(
+        approved=False,
+        message="Application rejected: Credit score too low even for the maximum period."
+    )
